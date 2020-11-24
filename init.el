8901ce951322d1817b69f82fb43ccadf8629316d
Siddhartha <sid@countvajhula.com>
2020-11-23 20:45:22 -0800

    extract evil keybinding interconnects (into epistemic)

--- init.el
+++ init.el
@@ -75,19 +75,6 @@
                         eem-lisp-tower
                         eem-emacs-tower)))
 
-  ;; evil interop keybindings
-  (define-key evil-normal-state-map [escape] 'eem-enter-higher-level)
-  (define-key evil-normal-state-map [return] 'eem-enter-lower-level)
-  (define-key evil-visual-state-map [escape] 'eem-enter-higher-level)
-  (define-key evil-visual-state-map [return] 'eem-enter-lower-level)
-  (define-key evil-replace-state-map [escape] 'eem-enter-higher-level)
-  (define-key evil-replace-state-map [return] 'eem-enter-lower-level)
-  (define-key evil-insert-state-map [escape] 'eem-enter-higher-level)
-  ;; TODO: this keybinding should be dependent on whether there are any
-  ;; other modes in the tower. If not, then this shouldn't be bound
-  ;; IOW this keybinding (and some class of bindings more generally)
-  ;; is tower-specific
-  (define-key evil-emacs-state-map [escape] 'eem-enter-higher-level)
 
   (setq epistemic-mode t)
 
