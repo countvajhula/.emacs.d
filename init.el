bc710f017d656ac356c22d696b54a6549e8417df
Siddhartha <sid@countvajhula.com>
2020-12-09 19:40:08 -0800

    rename eem -> rigpa

--- init.el
+++ init.el
@@ -41,22 +41,22 @@
   (setq rigpa-show-menus nil)
 
   (global-set-key (kbd "s-n") 'evil-normal-state)
-  (global-set-key (kbd "s-m s-m") 'eem-flashback-to-last-tower)
+  (global-set-key (kbd "s-m s-m") 'rigpa-flashback-to-last-tower)
   (global-set-key (kbd "C-<escape>") 'my-enter-tower-mode)
   (global-set-key (kbd "M-<escape>") 'my-enter-mode-mode) ;; TODO: s-esc and s-ret should operate based on a structure, shouldn't be hardcoded
   (global-set-key (kbd "s-<escape>") 'my-enter-mode-mode)
   (global-set-key (kbd "M-<return>")
                   (lambda ()
                     (interactive)
-                    (eem-enter-selected-level)
-                    (let ((ground (eem--get-ground-buffer)))
+                    (rigpa-enter-selected-level)
+                    (let ((ground (rigpa--get-ground-buffer)))
                       (my-exit-mode-mode)
                       (switch-to-buffer ground))))
   (global-set-key (kbd "s-<return>")
                   (lambda ()
                     (interactive)
-                    (eem-enter-selected-level)
-                    (let ((ground (eem--get-ground-buffer)))
+                    (rigpa-enter-selected-level)
+                    (let ((ground (rigpa--get-ground-buffer)))
                       (my-exit-mode-mode)
                       (switch-to-buffer ground))))
   (global-set-key (kbd "C-<return>")
@@ -68,66 +68,66 @@
   (global-set-key (kbd "s-y")        ; symex mode
                   (lambda ()
                     (interactive)
-                    (eem-enter-mode "symex"))) ; since y looks like inverted lambda
+                    (rigpa-enter-mode "symex"))) ; since y looks like inverted lambda
   (global-set-key (kbd "s-;") (kbd "s-y")) ; since y is hard to reach
   ;; TODO: probably add the symex config under symex once hooks are added
   (global-set-key (kbd "s-w")        ; window mode
                   (lambda ()
                     (interactive)
-                    (eem-enter-mode "window")))
+                    (rigpa-enter-mode "window")))
   (global-set-key (kbd "s-v")        ; view mode
                   (lambda ()
                     (interactive)
-                    (eem-enter-mode "view")))
+                    (rigpa-enter-mode "view")))
   (global-set-key (kbd "s-x")        ; char mode
                   (lambda ()
                     (interactive)
-                    (eem-enter-mode "char")))
+                    (rigpa-enter-mode "char")))
   (global-set-key (kbd "s-a")        ; activity mode
                   (lambda ()
                     (interactive)
-                    (eem-enter-mode "activity")))
+                    (rigpa-enter-mode "activity")))
   (global-set-key (kbd "s-z")        ; text mode
                   (lambda ()
                     (interactive)
-                    (eem-enter-mode "text")))
+                    (rigpa-enter-mode "text")))
   (global-set-key (kbd "s-g")        ; history mode
                   (lambda ()
                     (interactive)
-                    (eem-enter-mode "history")))
+                    (rigpa-enter-mode "history")))
   (global-set-key (kbd "s-i")        ; system mode
                   (lambda ()
                     (interactive)
-                    (eem-enter-mode "system")))
+                    (rigpa-enter-mode "system")))
   (global-set-key (kbd "s-b")        ; buffer mode
                   (lambda ()
                     (interactive)
-                    (eem-enter-mode "buffer")))
+                    (rigpa-enter-mode "buffer")))
   (global-set-key (kbd "s-f")        ; file mode
                   (lambda ()
                     (interactive)
-                    (eem-enter-mode "file")))
+                    (rigpa-enter-mode "file")))
   (global-set-key (kbd "s-t")        ; tab mode
                   (lambda ()
                     (interactive)
-                    (eem-enter-mode "tab")))
+                    (rigpa-enter-mode "tab")))
   (global-set-key (kbd "s-l")        ; line mode
                   (lambda ()
                     (interactive)
-                    (eem-enter-mode "line")))
+                    (rigpa-enter-mode "line")))
   (global-set-key (kbd "s-e")        ; application mode
                   (lambda ()
                     (interactive)
-                    (eem-enter-mode "application")))
+                    (rigpa-enter-mode "application")))
   (global-set-key (kbd "s-r")        ; word mode
                   (lambda ()
                     (interactive)
-                    (eem-enter-mode "word")))
+                    (rigpa-enter-mode "word")))
 
   ;; until arc is supported in symex mode
   (add-hook 'arc-mode-hook (lambda ()
-                             (setq eem--current-tower-index 2)
-                             (setq eem--current-level 2))))
+                             (setq rigpa--current-tower-index 2)
+                             (setq rigpa--current-level 2))))
 
 ;; load any customizations done via EMACS UI
 (load custom-file 'noerror)
