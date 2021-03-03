e1f13934c7ac4f094c6b3a19db7d413aae6b2274
Siddhartha <sid@countvajhula.com>
2021-03-02 17:54:44 -0800

    keybinding to insert a space without leaving normal mode

--- my-init/task-environment.el
+++ my-init/task-environment.el
@@ -41,6 +41,11 @@
   (define-key evil-insert-state-map [escape] 'evil-normal-state)
   ;; C-z goes to emacs mode in "insert" mode
   (define-key evil-insert-state-map (kbd "C-z") 'evil-emacs-state)
+  (define-key evil-normal-state-map ; convenient way to add a space
+    (kbd "C-SPC")
+    (lambda ()
+      (interactive)
+      (insert " ")))
 
   ;; so that W uses symbols
   ;; (while w uses words, and # and * use symbols)
