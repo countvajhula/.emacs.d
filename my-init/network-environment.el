59b284979582b9441b699b8936d6539058e71d22
Siddhartha <sid@countvajhula.com>
2021-06-25 11:10:18 -0700

    basic IRC config

--- my-init/network-environment.el
+++ my-init/network-environment.el
@@ -1,1 +1,9 @@
 ;; email, web, networking, phone, chat, IRC, etc.
+
+(use-package erc
+  :config
+  ;; See https://www.gnu.org/software/emacs/manual/html_node/erc/Sample-Configuration.html
+  ;; for more configuration options
+  (setq erc-nick "countvajhula")
+  (setq erc-server "irc.libera.chat")
+  (setq erc-autojoin-channels-alist '(("freenode.net" "#emacs"))))
