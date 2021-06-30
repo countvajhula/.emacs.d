1ec1d6bb58fe86ca4497de5eaa1c193edf2620ee
Siddhartha <sid@countvajhula.com>
2021-06-30 10:56:23 -0700

    IRC config..

--- my-init/network-environment.el
+++ my-init/network-environment.el
@@ -4,6 +4,8 @@
   :config
   ;; See https://www.gnu.org/software/emacs/manual/html_node/erc/Sample-Configuration.html
   ;; for more configuration options
+  ;; Also: https://www.reddit.com/r/emacs/comments/8ml6na/tip_how_to_make_erc_fun_to_use/
   (setq erc-nick "countvajhula")
   (setq erc-server "irc.libera.chat")
-  (setq erc-autojoin-channels-alist '(("freenode.net" "#emacs"))))
+  (setq erc-autojoin-channels-alist '(("freenode.net" "#emacs" "#racket")))
+  (setq erc-autojoin-channels-alist '(("libera.chat" "#emacs" "#racket"))))
