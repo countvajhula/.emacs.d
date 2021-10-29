7cdd647a902e12741d45d7f23d24fdab951f5899
Siddhartha <sid@countvajhula.com>
2021-10-29 16:51:50 -0700

    reorder symex quoting prefixes

--- my-init/application-environment.el
+++ my-init/application-environment.el
@@ -129,11 +129,10 @@
    :type git)
   :custom
   (symex-modal-backend 'evil)
-  (symex-quote-prefix-list (list "'" "`" "#'" "#`"))
+  (symex-quote-prefix-list (list "'" "#'" "`" "#`"))
   (symex-unquote-prefix-list (list "," ",@" "#,@"))
   :config
   (symex-initialize)
-  (symex-hide-menu)
   (define-key
     symex-mode-map
     (kbd "C-w")
