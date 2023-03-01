48af0da3cf369cc9c5e47c55a6880871ada33ab6
Siddhartha <sid@countvajhula.com>
2023-02-28 20:58:35 -0800

    subed config (for aligning video captions)

--- my-init/application-environment.el
+++ my-init/application-environment.el
@@ -506,6 +506,25 @@
   :config
   (beacon-mode 1))
 
+(use-package subed
+  :defer t
+  :straight
+  (subed
+   :type git
+   :host github
+   :repo "sachac/subed"
+   :files ("subed/*.el"))
+
+  :config
+  ;; Disable automatic movement of point by default
+  (add-hook 'subed-mode-hook 'subed-disable-sync-point-to-player)
+  ;; Remember cursor position between sessions
+  (add-hook 'subed-mode-hook 'save-place-local-mode)
+  ;; Break lines automatically while typing
+  (add-hook 'subed-mode-hook 'turn-on-auto-fill)
+  ;; Break lines at 40 characters
+  (add-hook 'subed-mode-hook (lambda () (setq-local fill-column 40))))
+
 (require 'my-familiar)
 
 (require 'my-general-behavior)
