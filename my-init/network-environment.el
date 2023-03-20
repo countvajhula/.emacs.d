8fb5d3873af74311b4e5a8581238790d4b428bc3
Siddhartha <sid@countvajhula.com>
2023-03-20 10:41:21 -0700

    note for reference

--- my-init/network-environment.el
+++ my-init/network-environment.el
@@ -5,6 +5,7 @@
   ;; See https://www.gnu.org/software/emacs/manual/html_node/erc/Sample-Configuration.html
   ;; for more configuration options
   ;; Also: https://www.reddit.com/r/emacs/comments/8ml6na/tip_how_to_make_erc_fun_to_use/
+  ;; Some config in .authinfo
   (setq erc-nick "countvajhula")
   (setq erc-server "irc.libera.chat")
   (setq erc-autojoin-channels-alist '(("freenode.net" "#emacs" "#racket")))
