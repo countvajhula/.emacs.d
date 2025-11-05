fc37e082e7b53e9fd6b091acdce8e0b61934e45d
Siddhartha <sid@countvajhula.com>
2025-11-04 17:55:22 -0800

    use elacarte to manage package recipes

--- my-init/setup.el
+++ my-init/setup.el
@@ -24,50 +24,31 @@
 (straight-use-package 'use-package)
 (setq straight-use-package-by-default t)
 
+(defun my-bootstrap-elacarte (recipes-file)
+  "Directly read the recipe for elacarte and install it.
+
+RECIPES is a file containing a list of recipes. Reading the recipes.eld
+files directly is necessary because a recipe repository that could be
+used by Straight doesn't yet exist."
+  (unless (file-exists-p recipes-file)
+    (user-error "Recipes file %s not found!" recipes-file))
+  (let* ((recipes (with-temp-buffer
+                    (insert-file-contents-literally recipes-file)
+                    ;; Use `read-from-string` to handle empty files gracefully.
+                    (car (read-from-string (buffer-string)))))
+         (recipe (alist-get 'elacarte recipes)))
+    (unless recipe
+      (user-error "Elacarte recipe missing in %s!" recipes-file))
+    (straight-use-package (cons 'elacarte recipe))))
+
+(my-bootstrap-elacarte
+ (expand-file-name "elacarte/recipes.eld"
+                   user-emacs-directory))
+
 ;; --- Add the local "xelpa" recipe repository ---
-
-;; 0. Generate XELPA from the local recipes.el
-(let* ((xelpa-dir (expand-file-name "xelpa" user-emacs-directory))
-       (source-recipe-file (expand-file-name "recipes.el" user-emacs-directory))
-       (generated-recipes-dir (expand-file-name "recipes" xelpa-dir)))
-
-  (when (file-exists-p source-recipe-file)
-    (message "--- Generating local 'XELPA' recipe repository ---")
-    ;; Clean and create the target directory.
-    (when (file-directory-p generated-recipes-dir)
-      (delete-directory generated-recipes-dir 'recursive))
-    (make-directory generated-recipes-dir 'parents)
-
-    ;; Read the list of recipes.
-    (let ((recipes (with-temp-buffer
-                     (insert-file-contents source-recipe-file)
-                     (read (current-buffer)))))
-      (dolist (recipe recipes)
-        (let* ((recipe-id (car recipe))
-               (package-name (if (symbolp recipe-id) (symbol-name recipe-id) recipe-id))
-               (target-file (expand-file-name package-name generated-recipes-dir)))
-          ;; Write each recipe to its own file inside the generated directory.
-          (with-temp-file target-file
-            (prin1 recipe (current-buffer))))))
-    (message "--- 'XELPA' generation complete ---")))
-
-;; 1. Tell straight.el to "install" the recipe repository package.
-;;    The `:defer t` keyword prevents use-package from
-;;    trying (and failing) to `require` a non-existent 'xelpa' feature.
-(use-package xelpa
-  :defer t
-  :straight
-  (xelpa
-   :type nil
-   :local-repo "~/.emacs.d/xelpa"
-   :build nil))
-
-;; 2. Manually add the path to the load-path and load the recipe protocol functions.
-(add-to-list 'load-path "~/.emacs.d/xelpa")
-(require 'xelpa)
-
-;; 3. Now that the protocol is loaded, register "xelpa" as a recipe source.
-(add-to-list 'straight-recipe-repositories 'xelpa)
+(require 'elacarte)
+(elacarte-build-recipe-repository)
+(elacarte-register-recipe-repository)
 
 ;; 'require' looks in the load-path, so packages need to be
 ;; downloaded from melpa prior to this.
